a7c5b691818791f9a624f74e61a6e15e1bb14c16
Demianturcan <demianturcan@iesebre.com>
2024-10-24 17:54:22 +0200

    base header i separació seccions

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,9 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
+import '../css/style.css'
+import javascriptLogo from '../javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
+/*
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -19,6 +20,7 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`
+
 
 setupCounter(document.querySelector('#counter'))
+    `*/
