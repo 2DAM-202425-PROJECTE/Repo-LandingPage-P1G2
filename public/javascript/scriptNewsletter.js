570b55b22e25ee3f2a314ef409ff239a89dff6d9
AlexanderBeltranFerreres <abeltran3@iesebre.com>
2024-11-29 19:38:59 +0100

    Afegida llibreria EmailJS i fet fun js enviarCorreu

--- public/javascript/scriptNewsletter.js
+++ public/javascript/scriptNewsletter.js
@@ -1,3 +1,5 @@
+import emailjs from '@emailjs/browser';
+
 function subscribeNewsletter(event) {
     event.preventDefault();
     const thankYouMessage = document.getElementById('thank-you-message');
@@ -13,6 +15,30 @@ function subscribeNewsletter(event) {
         newsletterContainer.classList.remove('hidden');
         closeNewsletterPopup();
     }, 3000);
+}
+
+emailjs.init('user_id_aqui'); //connectem al EmailJS
+
+function EnviarCorreu(event) {
+    event.preventDefault(); //prvenim que envie el formulari
+
+    // Obtenim el correu de l'usuari des del formulari
+    const userEmail = document.querySelector('input[type="email"]').value;
+    //Agafem un nom de usuari segons el correu que ems passen.
+    const userName = userEmail.split('@')[0];
+
+    // Passem els paràmetres del tempalte a EmailJS
+    const templateParams = {
+        to_name: userName,
+    };
+
+    // Enviem el correu passant el servei amb id i el template amb id
+    emailjs.send('service_id_aqui', 'template_id_aqui', templateParams)
+        .then((response) => {
+            console.log('Enviat Correctament:', response);
+        }, (error) => {
+            console.log('Error:', error);
+        });
 }
 
 function closeNewsletterPopup() {
